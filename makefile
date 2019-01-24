405d84bda4125fd0bd50c517e1e45a81e96d60c7
charlie <the_charlie_daly@hotmail.co.uk>
2019-01-24 01:03:20 +0000

    Almosst... there....

--- makefile
+++ makefile
@@ -6,3 +6,14 @@ run: build
 		-e FACEBOOK_TOKEN=$(FACEBOOK_TOKEN) \
 		-v $(PWD)/output:/output \
 		fb-photos
+
+download-chrome-driver:
+	curl -O https://chromedriver.storage.googleapis.com/2.45/chromedriver_linux64.zip && \
+	sudo unzip chromedriver_linux64.zip -d /usr/bin && \
+	rm ./chromedriver_linux64.zip
+
+download-firefox-driver:
+	curl -O -L "https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz" && \
+	tar -xvzf "geckodriver-v0.23.0-linux64.tar.gz" && \
+	sudo mv geckodriver /usr/bin/geckodriver && \
+	rm geckodriver-v0.23.0-linux64.tar.gz
--- run.py
+++ run.py
@@ -1,29 +1,99 @@
-import facebook
-import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from os import environ
+import re
 
-token = os.environ['FACEBOOK_TOKEN']
-graph = facebook.GraphAPI(access_token=token, version="3.1")
-me = graph.get_object(id='me', fields='id')
-id = me['id']
+user_name = environ["FACEBOOK_USERNAME"]
+password = environ["FACEBOOK_PASSWORD"]
 
-print('Using user id {}.'.format(id))
+class PageObject:
+    def __init__(self, driver):
+        self.driver = driver
 
-def get_cursor_url(user_id, next_cursor=None):
-    if next_cursor == None:
-        return '{}/photos?type=tagged&limit=25'.format(id) 
-    else:
-        return '{}/photos?type=tagged&limit=25&after={}'.format(id, next_cursor)
+    def _get_element_by_text(self, text):
+        xpath = "//*[contains(text(), '{}')]".format(text)
+        return driver.find_element_by_xpath(xpath)
 
-def download_image(id):
-    photo = graph.get_object(id='{}'.format(id), fields='images')
-    sorted_images = sorted(photo['images'], key=lambda value: -1 * value['height']) 
+    def _get_element_by_href(self, url):
+        return self.driver.find_element_by_css_selector('a[href="{}"]'.format(url))
+
+    def _get_element_of_type_with_text_matching_regex(self, tag, regex):
+        elements = self.driver.find_elements_by_tag_name(tag)
+        for element in elements:
+            result = re.search(regex, element.text)
+            if result is not None:
+                return element
+        return None
+
+    def _get_element_of_type_with_href_matching_regex(self, tag, regex):
+        elements = self.driver.find_elements_by_tag_name(tag)
+        for element in elements:
+            href = element.get_attribute("href")
+            if href is not None:
+                result = re.search(regex, href)
+                if result is not None:
+                    return element
+        return None
+
+class PhotoIteratorPageObject(PageObject):
+    def __init__(self, driver):
+        super(driver)
+        self.driver = driver
+        self.links = self.driver.find_elements_by_css_selector("a.ba")
+
+    def __iter__(self):
+        if len(self.links) > 0:
+            self.current_link = self.links[0]
+        else:
+            self.current_link = None
+        return self
+    
+    def __next__(self):
+        
+        self.current_link.click()
+        self._get_element_by_text("View full size").click()
+        image_src = self.driver.find_element_by_name("img").get_attribute("src")
+        return image_src
 
 
-images_page = graph.get_object(id='{}/photos?type=tagged'.format(id))
-data_block = images_page['data']
-next_page = images_page['paging']['cursors']['after']
+class FacebookPageObject(PageObject):
+    
+    def __init__(self, driver):
+        super(driver)
+        self.driver = driver
 
-print('Next page id: {}'.format(next_page))
+    def login(self, username, password):
+        
+        facebook_login_url = "https://m.facebook.com"
+        self.driver.get(facebook_login_url)
+        self.driver.find_element_by_id("m_login_email").send_keys(username)
+        self.driver.find_element_by_css_selector("input[name=pass]").send_keys(password)
+        self.driver.find_element_by_css_selector("input[name='login']").click()
+        # self.driver.find_element_by_css_selector('a[href="/login/save-device/cancel/?flow=interstitial_nux&nux_source=regular_login"]').click()
+        self._get_element_by_href("/login/save-device/cancel/?flow=interstitial_nux&nux_source=regular_login").click()
 
-next_page_all = graph.get_object(id='{}/photos?type=tagged&before={}'.format(id, next_page))
-print(next_page_all)
+    def go_to_photos(self, page=0):
+        self._get_element_by_text("Profile").click()
+        self._get_element_by_text("Photos").click()
+        element = self._get_element_of_type_with_text_matching_regex("a", "^See*")
+        element.click()
+        for _ in range(0, page):
+            self.driver.find_element_by_css_selector("#m_more_item a").click()
+
+    def get_photo_link_iterator(self):
+        links = self.driver.find_elements_by_css_selector("a.ba")
+
+driver = webdriver.Firefox()
+page = FacebookPageObject(driver)
+page.login(user_name, password)
+page.go_to_photos()
+
+print("done")
+# driver.get(facebook_login_url)
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
