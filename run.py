f8ec509a685a080f215fac2480c7e885fff03c54
charlie <the_charlie_daly@hotmail.co.uk>
2019-01-27 21:23:14 +0000

    Working with paging from tile page.

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from os import environ
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from os import environ, path
 import re
+import urllib
+import time
+import datetime
 
 user_name = environ["FACEBOOK_USERNAME"]
 password = environ["FACEBOOK_PASSWORD"]
@@ -35,31 +41,73 @@ class PageObject:
                     return element
         return None
 
+    def _wait_for_element(self, css_selector):
+        try:
+            _ = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
+        except:
+            raise "Not found: {}".format(css_selector)
+
 class PhotoIteratorPageObject(PageObject):
     def __init__(self, driver):
-        super(driver)
+        PageObject.__init__(self, driver)
         self.driver = driver
-        self.links = self.driver.find_elements_by_css_selector("a.ba")
+        self.index = 0
 
     def __iter__(self):
-        if len(self.links) > 0:
-            self.current_link = self.links[0]
-        else:
-            self.current_link = None
         return self
-    
-    def __next__(self):
-        
-        self.current_link.click()
-        self._get_element_by_text("View full size").click()
-        image_src = self.driver.find_element_by_name("img").get_attribute("src")
-        return image_src
 
+    def next(self):
+        if self._current_link() is None:
+            raise StopIteration()
+
+        self._current_link().click()
+        date_string = self.driver.find_element_by_tag_name("abbr").text
+        date = self._parse_date(date_string)
+        formatted_date_string = "{:%Y_%m_%d}".format(date)
+        self._get_element_by_text("View full size").click()
+        time.sleep(0.1)
+        tabs = self.driver.window_handles
+        self.driver.switch_to.window(tabs[1])
+        time.sleep(1)
+        image_src = self.driver.find_element_by_tag_name("img").get_attribute("src")
+        self.driver.close()
+        self.driver.switch_to.window(tabs[0])
+        self.driver.back()
+        self.index += 1
+        return (image_src, formatted_date_string)
+
+    def _parse_date(self, date_string):
+        for fmt in ('%d %b %Y', '%d %B %Y'):
+            try:
+                stripped = date_string.split(' at ')[0]
+                return datetime.datetime.strptime(stripped, fmt)
+            except ValueError:
+                pass
+        raise ValueError('no valid date format found')
+
+    def _current_link(self):
+        links = self.driver.find_elements_by_css_selector("a.ba")
+        if len(links) == 0:
+            return None
+        if self.index >= len(links) and not self._next_page():
+            return None
+        else:
+            links = self.driver.find_elements_by_css_selector("a.ba")
+        return links[self.index]
+
+    def _next_page(self):
+        next_page_link = self._get_element_by_text("See more photos")
+        if next_page_link is None:
+            return False
+        next_page_link.click()
+        self.index = 0
+        return True
 
 class FacebookPageObject(PageObject):
     
     def __init__(self, driver):
-        super(driver)
+        PageObject.__init__(self, driver)
         self.driver = driver
 
     def login(self, username, password):
@@ -67,7 +115,7 @@ class FacebookPageObject(PageObject):
         facebook_login_url = "https://m.facebook.com"
         self.driver.get(facebook_login_url)
         self.driver.find_element_by_id("m_login_email").send_keys(username)
-        self.driver.find_element_by_css_selector("input[name=pass]").send_keys(password)
+        self.driver.find_element_by_css_selector("input[name='pass']").send_keys(password)
         self.driver.find_element_by_css_selector("input[name='login']").click()
         # self.driver.find_element_by_css_selector('a[href="/login/save-device/cancel/?flow=interstitial_nux&nux_source=regular_login"]').click()
         self._get_element_by_href("/login/save-device/cancel/?flow=interstitial_nux&nux_source=regular_login").click()
@@ -81,12 +129,23 @@ class FacebookPageObject(PageObject):
             self.driver.find_element_by_css_selector("#m_more_item a").click()
 
     def get_photo_link_iterator(self):
-        links = self.driver.find_elements_by_css_selector("a.ba")
+        return PhotoIteratorPageObject(self.driver)
 
 driver = webdriver.Firefox()
 page = FacebookPageObject(driver)
 page.login(user_name, password)
 page.go_to_photos()
+count = 0
+
+for (link, name) in page.get_photo_link_iterator():
+    i = 0
+    full_name = "output/{}_{}.jpg".format(name, i)
+    while path.exists(full_name):
+        i += 1
+        full_name = "output/{}_{}.jpg".format(name, i)
+
+    urllib.urlretrieve(link, full_name)
+    print("Created {}.".format(full_name))
 
 print("done")
 # driver.get(facebook_login_url)
